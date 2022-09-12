27df7775494b9df90b12327a109bd7a3230675c3
Bruno Paré-Simard <bruno.p@brunops.com>
2022-09-12 00:01:12 -0400

    When creating a block, create also a `pyproject.toml`.
    This is to store the dependencies of specific block. Based on Polylith and their tool on Clojure, there is dependencies definition for components and bases.

--- poetry_polylith_plugin/components/blocks.py
+++ poetry_polylith_plugin/components/blocks.py
@@ -3,12 +3,15 @@ from pathlib import Path
 from poetry_polylith_plugin.components.dirs import create_dir
 from poetry_polylith_plugin.components.files import create_file
 from poetry_polylith_plugin.components.interfaces import create_interface
+from poetry_polylith_plugin.components.toml import create_empty_toml
 
 
 def create_block(
     path: Path, name: str, namespace: str, package: str, modulename: str = "core"
 ):
-    d = create_dir(path, f"{name}/{package}/src/{namespace}/{package}")
+    block_path = create_dir(path, f"{name}/{package}")
+    create_empty_toml(package, block_path)
 
-    create_file(d, f"{modulename}.py")
-    create_interface(d, namespace, package, modulename)
+    src_path = create_dir(block_path, f"src/{namespace}/{package}")
+    create_file(src_path, f"{modulename}.py")
+    create_interface(src_path, namespace, package, modulename)
