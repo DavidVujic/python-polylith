9ffad364ec7df6a2270e3fdc12e9720e6e7aee08
davidvujic <david@vujic.se>
2022-11-01 20:40:27 +0100

    fix: poly info counting bases, components and projects

--- components/polylith/component/get.py
+++ components/polylith/component/get.py
@@ -4,26 +4,13 @@ from pathlib import Path
 from polylith.component.constants import dir_name
 
 
-def get_component_dirs(path: Path, top_dir) -> Generator:
-    component_dir = path / top_dir
+def get_component_dirs(path: Path, top_dir, ns) -> Generator:
+    component_dir = path / top_dir / ns
 
     return (f for f in component_dir.iterdir() if f.is_dir())
 
 
-def dirs(path, ns) -> Generator:
-    return (f.name for f in path.glob(f"{ns}/{path.name}") if f.is_dir())
-
-
-def component_dirs(path: Path, ns: str) -> dict:
-    src_dirs = dirs(path, ns)
-
-    return {
-        "name": path.name,
-        "src": True if next(src_dirs, None) else False,
-    }
-
-
 def get_components_data(path: Path, ns: str, top_dir: str = dir_name) -> list[dict]:
-    dirs = get_component_dirs(path, top_dir)
+    dirs = get_component_dirs(path, top_dir, ns)
 
-    return [component_dirs(d, ns) for d in dirs]
+    return [{"name": d.name} for d in dirs]
