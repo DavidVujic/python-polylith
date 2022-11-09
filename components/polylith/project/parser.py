4f16b00e77436772d50e80ebf4d639ecaa2bd446
davidvujic <david@vujic.se>
2022-11-09 06:37:49 +0100

    wip: collect project bricks data

--- components/polylith/project/parser.py
+++ components/polylith/project/parser.py
@@ -1,44 +1,14 @@
 from pathlib import Path
 
-import tomlkit
-from polylith.project.get import (
-    get_project_files,
-    get_project_name,
-    get_project_package_includes,
-    get_toml,
-)
 
-
-def to_path(package: dict) -> dict[str, Path | str]:
+def to_path(package: dict) -> Path:
     include = package["include"]
     from_path = package.get("from")
 
-    combined_path = Path(f"{from_path}/{include}") if from_path else Path(include)
-
-    return {
-        "path": combined_path,
-        "brick": include,
-    }
+    return Path(f"{from_path}/{include}") if from_path else Path(include)
 
 
-def to_paths(toml: tomlkit.TOMLDocument) -> list[dict[str, Path | str]]:
-    packages = get_project_package_includes(toml)
-
+def parse_package_paths(packages: list[dict]) -> list[Path]:
     sorted_packages = sorted(packages, key=lambda p: (p["from"], p["include"]))
 
     return [to_path(p) for p in sorted_packages]
-
-
-def to_project_package_paths(toml: tomlkit.TOMLDocument) -> dict:
-    name = get_project_name(toml)
-    paths = to_paths(toml)
-
-    return {"project_name": name, "bricks": paths}
-
-
-def get_projects_package_paths(path: Path) -> list[dict]:
-    project_files = get_project_files(path)
-
-    tomls = (get_toml(f) for f in project_files)
-
-    return [to_project_package_paths(t) for t in tomls]
