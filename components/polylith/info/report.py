c278d79dc4c36744fc3bb6d0ab29b51a732f41e5
David Vujic <david@vujic.se>
2022-11-12 16:10:20 +0100

    feat: poly info command (#20)
    * feat: poly info with summary, and data about bricks in porjects
    
    * add rich dependency to poetry project

---
+++ components/polylith/info/report.py
@@ -0,0 +1,58 @@
+from rich import box
+from rich.columns import Columns
+from rich.console import Console
+from rich.padding import Padding
+from rich.table import Table
+from rich.theme import Theme
+
+info_theme = Theme(
+    {
+        "data": "#999966",
+        "proj": "#8A2BE2",
+        "comp": "#32CD32",
+        "base": "#6495ED",
+    }
+)
+
+
+def brick_status(brick, bricks) -> str:
+    status = ":heavy_check_mark:" if brick in bricks else "-"
+
+    return f"[data]{status}[/]"
+
+
+def print_bricks_in_projects(
+    projects_data: list[dict], bases_data: list[dict], components_data: list[dict]
+) -> None:
+    console = Console(theme=info_theme)
+    table = Table(box=box.SIMPLE_HEAD)
+    table.add_column("[data]brick[/]")
+
+    for project in projects_data:
+        project_name = project["name"]
+        table.add_column(f"[proj]{project_name}[/]")
+
+    components = sorted((c["name"] for c in components_data))
+    bases = sorted((b["name"] for b in bases_data))
+
+    for brick in components:
+        cols = [brick_status(brick, p.get("components")) for p in projects_data]
+        table.add_row(f"[comp]{brick}[/]", Columns(cols))
+
+    for brick in bases:
+        cols = [brick_status(brick, p.get("bases")) for p in projects_data]
+        table.add_row(f"[base]{brick}[/]", Columns(cols))
+
+    console.print(table, overflow="ellipsis")
+
+
+def print_workspace_summary(
+    projects_data: list[dict], bases_data: list[dict], components_data: list[dict]
+) -> None:
+    console = Console(theme=info_theme)
+
+    console.print(Padding("[data]Workspace summary[/]", (1, 0, 1, 0)))
+
+    console.print(f"[proj]projects[/]: [data]{len(projects_data)}[/]")
+    console.print(f"[comp]components[/]: [data]{len(components_data)}[/]")
+    console.print(f"[base]bases[/]: [data]{len(bases_data)}[/]")
