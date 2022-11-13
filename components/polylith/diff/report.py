ca2849c4821cd6f4db0322f01a1c40bda9466c07
davidvujic <david@vujic.se>
2022-11-13 14:13:29 +0100

    feat: poly diff command - with a short version to be used in CI

---
+++ components/polylith/diff/report.py
@@ -0,0 +1,71 @@
+from rich import box
+from rich.columns import Columns
+from rich.console import Console
+from rich.padding import Padding
+from rich.table import Table
+from rich.theme import Theme
+
+info_theme = Theme(
+    {
+        "data": "#999966",
+        "proj": "#8A2BE2",
+        "comp": "#32CD32",
+        "base": "#6495ED",
+    }
+)
+
+
+def brick_status(brick, bricks) -> str:
+    status = ":gear:" if brick in bricks else "-"
+
+    return f"[data]{status}[/]"
+
+
+def print_diff_details(
+    projects_data: list[dict], bases: list[str], components: list[str]
+) -> None:
+    console = Console(theme=info_theme)
+    table = Table(box=box.SIMPLE_HEAD)
+    table.add_column("[data]changed brick[/]")
+
+    for project in projects_data:
+        project_name = project["name"]
+        table.add_column(f"[proj]{project_name}[/]")
+
+    for brick in sorted(components):
+        cols = [brick_status(brick, p.get("components")) for p in projects_data]
+        table.add_row(f"[comp]{brick}[/]", Columns(cols))
+
+    for brick in sorted(bases):
+        cols = [brick_status(brick, p.get("bases")) for p in projects_data]
+        table.add_row(f"[base]{brick}[/]", Columns(cols))
+
+    console.print(table, overflow="ellipsis")
+
+
+def print_detected_changes_in_projects(projects: list[str]) -> None:
+    if not projects:
+        return
+
+    console = Console(theme=info_theme)
+
+    for project in sorted(projects):
+        console.print(f"[data]:gear: Changes found in [/][proj]{project}[/]")
+
+
+def print_diff_summary(
+    tag: str,  bases: list[str], components: list[str]
+) -> None:
+    console = Console(theme=info_theme)
+
+    console.print(Padding(f"[data]Diff: based on the {tag} tag[/]", (1, 0, 1, 0)))
+
+    if not bases and not components:
+        console.print("[data]No changes found.[/]")
+        return
+
+    if components:
+        console.print(f"[comp]Changed components[/]: [data]{len(components)}[/]")
+
+    if bases:
+        console.print(f"[base]Changed bases[/]: [data]{len(bases)}[/]")
--- components/polylith/poetry/commands/diff.py
+++ components/polylith/poetry/commands/diff.py
@@ -1,13 +1,22 @@
-import subprocess
 from pathlib import Path
 
+from cleo.helpers import option
 from poetry.console.commands.command import Command
-from polylith import repo, workspace
+from polylith import diff, info, repo
 
 
 class DiffCommand(Command):
     name = "poly diff"
     description = "Shows changed bricks compared to the latest git tag."
+
+    options = [
+        option(
+            long_name="short",
+            short_name="s",
+            description="Print only changed projects",
+            flag=True,
+        ),
+    ]
 
     def handle(self) -> int:
         root = repo.find_workspace_root(Path.cwd())
@@ -16,14 +25,24 @@ class DiffCommand(Command):
                 "Didn't find the workspace root. Expected to find a workspace.toml file."
             )
 
-        tag_pattern = workspace.parser.get_git_tag_pattern_from_config(root)
-        res = subprocess.run(
-            ["git", "tag", "-l", "--sort=-committerdate", f"{tag_pattern}"],
-            capture_output=True,
-        )
+        tag = diff.collect.get_latest_tag(root)
 
-        latest_tag = next((tag for tag in res.stdout.decode("utf-8").split()), None)
+        if not tag:
+            self.line("No tags found in repository.")
+        else:
+            files = diff.collect.get_files(tag)
+            bases = diff.collect.get_changed_bases(root, files)
+            components = diff.collect.get_changed_components(root, files)
+            projects = diff.collect.get_changed_projects(files)
+            projects_data = info.get_bricks_in_projects(root)
 
-        self.line(f"TAG: {latest_tag}")
+            short = self.option("short")
+
+            if short:
+                self.line(",".join(projects))
+            else:
+                diff.report.print_diff_summary(tag, bases, components)
+                diff.report.print_detected_changes_in_projects(projects)
+                diff.report.print_diff_details(projects_data, bases, components)
 
         return 0
