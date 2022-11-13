1e3537a096f7ba16e6c532108edde992c6ab167b
David Vujic <david@vujic.se>
2022-11-13 21:27:22 +0100

    fix: poly info depending on theme (#26)

--- components/polylith/bricks/component.py
+++ components/polylith/bricks/component.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from polylith.bricks.brick import create_brick
 from polylith.repo import components_dir
 from polylith.test import create_test
+from polylith import workspace
 
 
 def create_component(path: Path, namespace: str, package: str) -> None:
@@ -10,8 +11,11 @@ def create_component(path: Path, namespace: str, package: str) -> None:
     create_test(path, components_dir, namespace, package)
 
 
-def get_component_dirs(path: Path, top_dir, ns) -> list:
-    component_dir = path / top_dir / ns
+def get_component_dirs(root: Path, top_dir, ns) -> list:
+    theme = workspace.parser.get_theme_from_config(root)
+    dirs = top_dir if theme == "tdd" else f"{top_dir}/{ns}"
+
+    component_dir = root / dirs
 
     if not component_dir.exists():
         return []
@@ -20,8 +24,8 @@ def get_component_dirs(path: Path, top_dir, ns) -> list:
 
 
 def get_components_data(
-    path: Path, ns: str, top_dir: str = components_dir
+    root: Path, ns: str, top_dir: str = components_dir
 ) -> list[dict]:
-    dirs = get_component_dirs(path, top_dir, ns)
+    dirs = get_component_dirs(root, top_dir, ns)
 
     return [{"name": d.name} for d in dirs]
