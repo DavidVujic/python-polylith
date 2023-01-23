a4e46242364c6f065d90d4fd6c254ebe2b0e0243
David Vujic <david@vujic.se>
2023-01-23 17:32:21 +0100

    fix: create-command with name as path (#47)
    * fix(create-commands): create valid python file for names that specify a sub-path
    
    * bump version to 1.1.1

--- components/polylith/interface/interfaces.py
+++ components/polylith/interface/interfaces.py
@@ -9,11 +9,19 @@ __all__ = ["{modulename}"]
 """
 
 
+def to_namespaced_path(package: str) -> str:
+    parts = package.split("/")
+
+    return ".".join(parts)
+
+
 def create_interface(path: Path, namespace: str, package: str, modulename: str) -> None:
     interface = create_file(path, "__init__.py")
 
+    package_path = to_namespaced_path(package)
+
     content = template.format(
-        namespace=namespace, package=package, modulename=modulename
+        namespace=namespace, package=package_path, modulename=modulename
     )
 
     interface.write_text(content)
