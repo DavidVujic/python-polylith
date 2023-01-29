4aa927612d1b1af488dcddcc6474ea70ec80e571
davidvujic <david@vujic.se>
2023-01-29 16:18:08 +0100

    wip(poly libs): get all third-party libraries that are used in a Polylith repo

--- components/polylith/libs/grouping.py
+++ components/polylith/libs/grouping.py
@@ -1,10 +1,10 @@
 import sys
-from typing import Set
 from pathlib import Path
+from typing import List, Set
 
 from polylith import repo, workspace
-from polylith.libs.stdlib import standard_libs
 from polylith.libs.imports import list_imports
+from polylith.libs.stdlib import standard_libs
 
 
 def get_python_version() -> str:
@@ -15,14 +15,10 @@ def get_standard_libs(python_version: str) -> Set[str]:
     return standard_libs.get(python_version, set())
 
 
-def get_all_imports(root: Path) -> Set[str]:
-    components_dir = root / "components"
-    bases_dir = root / "bases"
+def fetch_all_imports(paths: List[Path]) -> Set[str]:
+    res = (list_imports(p) for p in paths)
 
-    a = list_imports(components_dir)
-    b = list_imports(bases_dir)
-
-    return a.union(b)
+    return set().union(*res)
 
 
 def extract_top_ns(imp: str) -> str:
@@ -31,8 +27,8 @@ def extract_top_ns(imp: str) -> str:
     return res[0]
 
 
-def extract_top_ns_from_all_imports(root: Path) -> Set[str]:
-    all_imports = get_all_imports(root)
+def extract_top_ns_from_all_imports(paths: List[Path]) -> Set[str]:
+    all_imports = fetch_all_imports(paths)
 
     return {extract_top_ns(imp) for imp in all_imports}
 
@@ -43,10 +39,13 @@ def get_all_third_party_imports() -> Set[str]:
     if not root:
         raise ValueError("Could not find the repo root")
 
+    components_dir = root / "components"
+    bases_dir = root / "bases"
+
     python_version = get_python_version()
     std_libs = get_standard_libs(python_version)
     top_ns = workspace.parser.get_namespace_from_config(root)
 
-    imports = extract_top_ns_from_all_imports(root)
+    imports = extract_top_ns_from_all_imports([components_dir, bases_dir])
 
     return imports - std_libs.union({top_ns})
