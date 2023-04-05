449f00e3ac500abaa03d4fc090976a269e9b9736
David Vujic <david@vujic.se>
2023-04-05 18:07:23 +0200

    fix: stdlib checks with less duplications and added extras (#86)
    * fix(stdlibs): add missing common extras + refactor duplicated code
    
    * bump version to 1.5.2
    
    * test(stdlib): check for extras
    
    * fix: no need to be explicit about none returns in tests

---
+++ test/components/polylith/libs/test_stdlib.py
@@ -0,0 +1,32 @@
+from polylith.libs import stdlib
+
+
+def test_stdlib_extras():
+    py = stdlib.standard_libs["3.11"]
+
+    assert "__future__" in py
+    assert "pkg_resources" in py
+
+
+def test_stdlib_3_9():
+    py38 = stdlib.standard_libs["3.8"]
+    py39 = stdlib.standard_libs["3.9"]
+
+    assert py38.difference(py39) == {"_dummy_thread", "dummy_threading"}
+    assert py39.difference(py38) == {"graphlib", "zoneinfo"}
+
+
+def test_stdlib_3_10():
+    py39 = stdlib.standard_libs["3.9"]
+    py310 = stdlib.standard_libs["3.10"]
+
+    assert py39.difference(py310) == {"formatter", "parser", "symbol"}
+    assert py310.difference(py39) == {"idlelib"}
+
+
+def test_stdlib_3_11():
+    py310 = stdlib.standard_libs["3.10"]
+    py311 = stdlib.standard_libs["3.11"]
+
+    assert py310.difference(py311) == {"binhex"}
+    assert py311.difference(py310) == {"tomllib"}
