e321e10b331bf47e5c45797ee8f981bf4c12dc59
davidvujic <david@vujic.se>
2023-04-16 12:21:05 +0200

    wip(poly sync): add sync command and component

--- test/components/polylith/sync/test_update.py
+++ test/components/polylith/sync/test_update.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import tomlkit
 from polylith.sync import update
 
@@ -5,19 +7,34 @@ from polylith.sync import update
 def test_brick_to_pyproject_package():
     ns = "unit_test"
     brick = "greet"
-    brick_folder = "components"
+    brick_type = "components"
+    loose_theme = "loose"
+    tdd_theme = "tdd"
 
-    expected_dev = {"include": f"{ns}/{brick}", "from": brick_folder}
-    expected_proj = {"include": f"{ns}/{brick}", "from": f"../../{brick_folder}"}
+    expected_dev = {"include": f"{ns}/{brick}", "from": brick_type}
+    expected_proj = {"include": f"{ns}/{brick}", "from": f"../../{brick_type}"}
 
-    res_dev = update.to_package(ns, brick, brick_folder, False)
-    res_proj = update.to_package(ns, brick, brick_folder, True)
+    expected_tdd_dev = {"include": f"{ns}/{brick}", "from": f"{brick_type}/{brick}/src"}
+    expected_tdd_proj = {
+        "include": f"{ns}/{brick}",
+        "from": f"../../{brick_type}/{brick}/src",
+    }
+
+    res_dev = update.to_package(ns, brick, brick_type, loose_theme, False)
+    res_proj = update.to_package(ns, brick, brick_type, loose_theme, True)
+
+    res_tdd_dev = update.to_package(ns, brick, brick_type, tdd_theme, False)
+    res_tdd_proj = update.to_package(ns, brick, brick_type, tdd_theme, True)
 
     assert res_dev == expected_dev
     assert res_proj == expected_proj
 
+    assert res_tdd_dev == expected_tdd_dev
+    assert res_tdd_proj == expected_tdd_proj
+
 
 def test_bricks_to_pyproject_packages():
+    root = Path.cwd()
     ns = "unit_test"
     base = "hello"
     component = "world"
@@ -27,7 +44,7 @@ def test_bricks_to_pyproject_packages():
         {"include": f"{ns}/{component}", "from": "components"},
     ]
 
-    res = update.to_packages(ns, {base}, {component}, False)
+    res = update.to_packages(root, ns, {base}, {component}, False)
 
     assert res == expected
 
