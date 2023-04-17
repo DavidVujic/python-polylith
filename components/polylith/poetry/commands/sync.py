c825de9ad8f8a0a4d01e693e60c6ebd3f2b190db
davidvujic <david@vujic.se>
2023-04-17 08:25:52 +0200

    wip(poly sync): add sync command and component

--- components/polylith/poetry/commands/sync.py
+++ components/polylith/poetry/commands/sync.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 from poetry.console.commands.command import Command
 from polylith import info, project, repo, sync, workspace
@@ -7,6 +8,21 @@ from polylith import info, project, repo, sync, workspace
 class SyncCommand(Command):
     name = "poly sync"
     description = "Update <comment>pyproject.toml</comment> with missing bricks."
+
+    def filter_projects_data(self, all_projects_data: List[dict]) -> List[dict]:
+        if self.option("directory"):
+            project_name = project.get_project_name(self.poetry.pyproject.data)
+
+            data = next(
+                (p for p in all_projects_data if p["name"] == project_name), None
+            )
+
+            if not data:
+                raise ValueError(f"Didn't find project in {self.option('directory')}")
+
+            return [data]
+
+        return all_projects_data
 
     def handle(self) -> int:
         root = repo.find_workspace_root(Path.cwd())
@@ -20,24 +36,15 @@ class SyncCommand(Command):
 
         bases = info.get_bases(root, ns)
         components = info.get_components(root, ns)
-
-        projects_data = info.get_bricks_in_projects(root, components, bases, ns)
         workspace_data = {"bases": bases, "components": components}
 
-        if self.option("directory"):
-            project_name = project.get_project_name(self.poetry.pyproject.data)
+        all_projects_data = info.get_bricks_in_projects(root, components, bases, ns)
+        projects_data = self.filter_projects_data(all_projects_data)
 
-            data = next((p for p in projects_data if p["name"] == project_name), None)
-
-            if not data:
-                raise ValueError(f"Didn't find project in {self.option('directory')}")
-
-            diffs = [sync.calculate_diff(root, ns, data, workspace_data)]
-        else:
-            diffs = [
-                sync.calculate_diff(root, ns, data, workspace_data)
-                for data in projects_data
-            ]
+        diffs = [
+            sync.calculate_diff(root, ns, data, workspace_data)
+            for data in projects_data
+        ]
 
         for diff in diffs:
             sync.report.print_summary(diff)
