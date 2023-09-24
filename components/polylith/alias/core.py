e2341e409112a3b63daa864d97b1829e92b9f372
davidvujic <david@vujic.se>
2023-09-24 18:05:42 +0200

    feat(poly check and libs): add --alias option, useful when an import differ from the library name

--- components/polylith/alias/core.py
+++ components/polylith/alias/core.py
@@ -1,8 +1,8 @@
 from functools import reduce
-from typing import List, Set
+from typing import Dict, List, Set
 
 
-def _to_key_with_values(acc: dict, alias: str) -> dict:
+def _to_key_with_values(acc: Dict, alias: str) -> Dict:
     k, v = str.split(alias, "=")
 
     values = [str.strip(val) for val in str.split(v, ",")]
@@ -10,12 +10,12 @@ def _to_key_with_values(acc: dict, alias: str) -> dict:
     return {**acc, **{k: values}}
 
 
-def parse(aliases: list[str]) -> dict[str, List[str]]:
+def parse(aliases: List[str]) -> Dict[str, List[str]]:
     """Parse a list of aliases defined as key=value(s) into a dictionary"""
     return reduce(_to_key_with_values, aliases, {})
 
 
-def pick(aliases: dict[str, List[str]], keys: Set) -> Set:
+def pick(aliases: Dict[str, List[str]], keys: Set) -> Set:
     matrix = [v for k, v in aliases.items() if k in keys]
 
     flattened: List = sum(matrix, [])
