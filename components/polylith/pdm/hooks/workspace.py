46ab02f261b275c314cf406c2ce1328bc3a2f109
David Vujic <david@vujic.se>
2024-02-04 23:00:54 +0100

    wip(pdm-workspace): write tdd theme paths to the pth file

--- components/polylith/pdm/hooks/workspace.py
+++ components/polylith/pdm/hooks/workspace.py
@@ -3,36 +3,36 @@ from typing import Set
 
 from polylith import toml, workspace
 
-defaults = {"bases", "components"}
 
-
-def paths_from_config(root: Path, config_data: dict) -> Set[str]:
-    ns = workspace.parser.get_namespace_from_config(root)
-    packages = toml.get_project_package_includes(ns, config_data)
+def paths_from_config(root: Path, ns: str, data: dict) -> Set[str]:
+    packages = toml.get_project_package_includes(ns, data)
 
     return {p["from"] for p in packages}
 
 
-def parse_paths(root: Path, config_data: dict) -> Set[str]:
-    theme = workspace.parser.get_theme_from_config(root)
+def parse_paths(root: Path, theme: str, ns: str, data: dict) -> Set[str]:
+    defaults = {"bases", "components"}
 
-    paths = defaults if theme == "loose" else paths_from_config(root, config_data)
+    paths = defaults if theme == "loose" else paths_from_config(root, ns, data)
 
     return {(root / p).as_posix() for p in paths}
 
 
-def write_pth_file(file_path: str, rows: Set[str]) -> None:
-    with open(file_path, "w") as f:
-        for row in rows:
-            f.write(f"{row}\n")
+def write_pth_file(build_dir: Path, paths: Set[str]) -> None:
+    filepath = build_dir / "polylith_workspace.pth"
+
+    with open(filepath, "w") as f:
+        for p in paths:
+            f.write(f"{p}\n")
 
 
 def build_initialize(config_data: dict, build_dir: Path, root: Path) -> None:
-    paths = parse_paths(root, config_data)
+    theme = workspace.parser.get_theme_from_config(root)
+    ns = workspace.parser.get_namespace_from_config(root)
+
+    paths = parse_paths(root, theme, ns, config_data)
 
     if not paths:
         return
 
-    filepath = build_dir / "polylith_workspace.pth"
-
-    write_pth_file(filepath.as_posix(), paths)
+    write_pth_file(build_dir, paths)
