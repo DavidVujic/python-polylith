fd71a49a28c6fbb27396a2c7bc2cb9547ada14e4
David Vujic <david@vujic.se>
2024-03-15 18:08:10 +0100

    WIP: compare project dependencies

--- components/polylith/poetry/commands/libs.py
+++ components/polylith/poetry/commands/libs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from cleo.helpers import option
 from poetry.console.commands.command import Command
 from polylith import commands, configuration, info, repo
 from polylith.poetry.commands.check import command_options
@@ -10,13 +11,18 @@ class LibsCommand(Command):
     name = "poly libs"
     description = "Show third-party libraries used in the workspace."
 
-    options = command_options
+    options = command_options + [
+        option(
+            long_name="short",
+            short_name="s",
+            description="Display Workspace Info adjusted for many projects",
+            flag=True,
+        ),
+    ]
 
-    def print_report(self, root: Path, ns: str, data: dict) -> bool:
+    def print_report(self, root: Path, ns: str, data: dict, options: dict) -> bool:
         name = data["name"]
         path = data["path"]
-
-        options = {"strict": self.option("strict"), "alias": self.option("alias")}
 
         try:
             third_party_libs = find_third_party_libs(self.poetry, path)
@@ -31,6 +37,12 @@ class LibsCommand(Command):
             return False
 
     def handle(self) -> int:
+        options = {
+            "strict": self.option("strict"),
+            "alias": self.option("alias"),
+            "short": self.option("short"),
+        }
+
         directory = self.option("directory")
         root = repo.get_workspace_root(Path.cwd())
         ns = configuration.get_namespace_from_config(root)
@@ -38,8 +50,8 @@ class LibsCommand(Command):
         all_projects_data = info.get_projects_data(root, ns)
         projects_data = filter_projects_data(self.poetry, directory, all_projects_data)
 
-        results = {self.print_report(root, ns, data) for data in projects_data}
+        results = {self.print_report(root, ns, data, options) for data in projects_data}
 
-        commands.libs.library_versions(all_projects_data, projects_data)
+        commands.libs.library_versions(all_projects_data, projects_data, options)
 
         return 0 if all(results) else 1
