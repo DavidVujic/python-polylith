cf33f7e6db75f4a9046c2f466998dad3d9682f72
Peder Landsverk <peder.landsverk@soprasteria.com>
2024-04-23 16:32:18 +0200

    Make report work even though a component is not imported by any other.

--- components/polylith/deps/report.py
+++ components/polylith/deps/report.py
@@ -21,7 +21,7 @@ def brick_status(bricks: List[str], brick_name: str, imported: str) -> str:
 
 
 def to_row(name: str, tag: str, brick_imports: dict, imported: List[str]) -> List[str]:
-    bricks = brick_imports[name]
+    bricks = brick_imports.get(name) or set() 
     statuses = [brick_status(bricks, name, i) for i in imported]
 
     return [f"[{tag}]{name}[/]"] + statuses
