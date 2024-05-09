5b91cdf62953a64545053479322ad91d3ab3aa53
David Vujic <david@vujic.se>
2024-05-09 11:44:43 +0200

    fix(pdm brick hook): validate included bricks

--- components/polylith/pdm/hooks/bricks.py
+++ components/polylith/pdm/hooks/bricks.py
@@ -4,14 +4,15 @@ from polylith import toml
 from polylith.pdm import core
 
 
-def build_initialize(config_data: dict, build_dir: Path) -> None:
+def build_initialize(root: Path, config_data: dict, build_dir: Path) -> None:
     bricks = toml.get_project_packages_from_polylith_section(config_data)
+    found_bricks = {k: v for k, v in bricks.items() if Path(root / k).exists()}
+
+    if not bricks or not found_bricks:
+        return
+
     top_ns = toml.get_custom_top_namespace_from_polylith_section(config_data)
     work_dir = core.get_work_dir(config_data)
-
-    if not bricks:
-        print("No bricks found.")
-        return
 
     if not top_ns:
         core.copy_bricks_as_is(bricks, build_dir)
