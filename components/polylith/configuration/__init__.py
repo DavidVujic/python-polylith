24c0f035f039f20984e7ace7af59dce6e51a2098
David Vujic <david@vujic.se>
2024-06-06 13:02:48 +0200

    fix(linting): add new sort options from config function to the __all__ list

--- components/polylith/configuration/__init__.py
+++ components/polylith/configuration/__init__.py
@@ -15,6 +15,7 @@ __all__ = [
     "get_namespace_from_config",
     "get_resources_structure_from_config",
     "get_tag_pattern_from_config",
+    "get_tag_sort_options_from_config",
     "get_tests_structure_from_config",
     "get_theme_from_config",
     "is_readme_generation_enabled",
