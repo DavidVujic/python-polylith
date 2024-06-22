18a46f51d5f32cddc532ef850da6941d11fa2460
David Vujic <david@vujic.se>
2024-06-22 10:21:37 +0200

    test(libs): add test data for lock-file created with pip-tools

--- test/components/polylith/libs/test_parse_lock_file.py
+++ test/components/polylith/libs/test_parse_lock_file.py
@@ -4,11 +4,27 @@ from polylith.libs import lock_files
 
 project_data = {"path": Path("./test/test_data")}
 
+expected_libraries = {
+    "annotated-types",
+    "anyio",
+    "click",
+    "fastapi",
+    "h11",
+    "idna",
+    "pydantic",
+    "pydantic-core",
+    "sniffio",
+    "starlette",
+    "typing-extensions",
+    "uvicorn",
+}
+
 
 def test_find_lock_file():
     expected = {
         "pdm.lock": "toml",
         "requirements.lock": "text",
+        "requirements.txt": "text",
     }
 
     res = lock_files.find_lock_file(project_data)
@@ -17,44 +33,20 @@ def test_find_lock_file():
 
 
 def test_parse_contents_of_rye_lock_file():
-    expected = {
-        "annotated-types",
-        "anyio",
-        "click",
-        "fastapi",
-        "h11",
-        "idna",
-        "pydantic",
-        "pydantic-core",
-        "sniffio",
-        "starlette",
-        "typing-extensions",
-        "uvicorn",
-    }
-
     names = lock_files.extract_lib_names(project_data, {"requirements.lock": "text"})
 
-    assert names == expected
+    assert names == expected_libraries
 
 
 def test_parse_contents_of_pdm_lock_file():
-    expected = {
-        "annotated-types",
-        "anyio",
-        "click",
-        "colorama",
-        "exceptiongroup",
-        "fastapi",
-        "h11",
-        "idna",
-        "pydantic",
-        "pydantic-core",
-        "sniffio",
-        "starlette",
-        "typing-extensions",
-        "uvicorn",
-    }
+    expected = {*expected_libraries, *{"colorama", "exceptiongroup"}}
 
     names = lock_files.extract_lib_names(project_data, {"pdm.lock": "toml"})
 
     assert names == expected
+
+
+def test_parse_contents_of_pip_tools_lock_file():
+    names = lock_files.extract_lib_names(project_data, {"requirements.txt": "text"})
+
+    assert names == expected_libraries
