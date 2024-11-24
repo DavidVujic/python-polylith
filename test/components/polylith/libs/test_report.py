44357debb3f5f8ed1467da55608ddfa83807bc3f
David Vujic <david@vujic.se>
2024-11-24 12:04:30 +0100

    refactor(libs test): less duplications

--- test/components/polylith/libs/test_report.py
+++ test/components/polylith/libs/test_report.py
@@ -1,4 +1,14 @@
+import pytest
 from polylith.libs import report
+
+third_party_libs = {
+    "cleo",
+    "mypy-extensions",
+    "poetry",
+    "tomlkit",
+    "requests",
+    "rich",
+}
 
 
 def test_calculate_diff_reports_no_diff():
@@ -9,13 +19,6 @@ def test_calculate_diff_reports_no_diff():
             "two": {"rich", "cleo"},
             "thre": {"tomlkit"},
         },
-    }
-
-    third_party_libs = {
-        "tomlkit",
-        "cleo",
-        "requests",
-        "rich",
     }
 
     res = report.calculate_diff(brick_imports, third_party_libs)
@@ -35,67 +38,33 @@ def test_calculate_diff_should_report_missing_dependency():
         },
     }
 
-    third_party_libs = {
-        "tomlkit",
-        "poetry",
-        "mypy-extensions",
-        "rich",
-    }
-
     res = report.calculate_diff(brick_imports, third_party_libs)
 
     assert res == {expected_missing}
 
 
-def test_calculate_diff_should_identify_close_match():
-    brick_imports = {
-        "bases": {"my_base": {"poetry"}},
-        "components": {
-            "one": {"tomlkit"},
-            "two": {"tomlkit", "aws_lambda_powertools", "rich"},
-            "three": {"rich", "pyyoutube"},
-        },
-    }
-
-    third_party_libs = {
-        "tomlkit",
-        "python-youtube",
-        "poetry",
-        "aws-lambda-powertools",
-        "rich",
-    }
-
-    res = report.calculate_diff(brick_imports, third_party_libs)
-
-    assert len(res) == 0
-
-
-def test_calculate_diff_should_identify_close_match_case_insensitive():
-    brick_imports = {
-        "bases": {"my_base": {}},
-        "components": {
-            "one": {"PIL"},
-        },
-    }
-
-    third_party_libs = {"pillow"}
-
-    res = report.calculate_diff(brick_imports, third_party_libs)
-
-    assert len(res) == 0
-
-
-def test_calculate_diff_strict_should_identify_close_match_for_dash_and_low_dash():
+@pytest.mark.parametrize(
+    "imports, is_strict",
+    [
+        ({"aws_lambda_powertools", "PIL", "pyyoutube"}, False),
+        ({"typing_extensions"}, True),
+    ],
+)
+def test_calculate_diff_should_identify_close_match(imports: set, is_strict: bool):
     brick_imports = {
         "bases": {"thebase": {"typer"}},
-        "components": {
-            "one": {"typing_extensions"},
-        },
+        "components": {"one": imports},
     }
 
-    third_party_libs = {"typer", "typing-extensions"}
+    libs = {
+        "aws-lambda-powertools",
+        "pillow",
+        "python-youtube",
+        "typer",
+        "typing-extensions",
+    }
 
-    res = report.calculate_diff(brick_imports, third_party_libs, True)
+    res = report.calculate_diff(brick_imports, libs, is_strict)
 
     assert len(res) == 0
 
