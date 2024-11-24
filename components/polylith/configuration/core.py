0c7b67ad9bf91583dd6aa241d6fc6af4ef1a20db
David Vujic <david@vujic.se>
2024-11-24 15:24:13 +0100

    feat: find workspace root, for repos with several sub-workspaces (#296)
    * feat: find workspace root, for repos without a workspace.toml and for repos where the Workspace root is not the same as the git root
    
    * fix: error message when workspace not found
    
    * bump Hatch hook to 1.2.9
    
    * bump PDM workspace and brick hooks to 1.1.0
    
    * bump Poetry plugin to 1.34.0
    
    * bump CLI to 1.23.0
    
    * refactor(libs test): less duplications
    
    * fix(workspace root): check if workspace.toml contain the expected config, otherwise return pyproject contents

--- components/polylith/configuration/core.py
+++ components/polylith/configuration/core.py
@@ -1,25 +1,11 @@
-from functools import lru_cache
 from pathlib import Path
 from typing import List, Union
 
-import tomlkit
 from polylith import repo
 
 
-@lru_cache
-def _load_workspace_config(path: Path) -> tomlkit.TOMLDocument:
-    fullpath = path / repo.workspace_file
-
-    if not fullpath.exists():
-        fullpath = path / repo.default_toml
-
-    content = fullpath.read_text()
-
-    return tomlkit.loads(content)
-
-
 def get_namespace_from_config(path: Path) -> str:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     return toml["tool"]["polylith"]["namespace"]
 
@@ -30,7 +16,7 @@ def get_git_tag_pattern(toml: dict) -> str:
 
 
 def get_tag_pattern_from_config(path: Path, key: Union[str, None]) -> Union[str, None]:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     patterns = toml["tool"]["polylith"].get("tag", {}).get("patterns")
 
@@ -41,7 +27,7 @@ def get_tag_pattern_from_config(path: Path, key: Union[str, None]) -> Union[str,
 
 
 def get_tag_sort_options_from_config(path: Path) -> List[str]:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     options = toml["tool"]["polylith"].get("tag", {}).get("sorting")
     # Default sorting option
@@ -51,21 +37,21 @@ def get_tag_sort_options_from_config(path: Path) -> List[str]:
 
 
 def is_test_generation_enabled(path: Path) -> bool:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     enabled = toml["tool"]["polylith"]["test"]["enabled"]
     return bool(enabled)
 
 
 def is_readme_generation_enabled(path: Path) -> bool:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     enabled = toml["tool"]["polylith"].get("resources", {}).get("brick_docs_enabled")
     return bool(enabled)
 
 
 def get_theme_from_config(path: Path) -> str:
-    toml: dict = _load_workspace_config(path)
+    toml: dict = repo.load_workspace_config(path)
 
     return toml["tool"]["polylith"]["structure"].get("theme") or "tdd"
 
--- components/polylith/repo/repo.py
+++ components/polylith/repo/repo.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
 from pathlib import Path
 from typing import Union
+
+import tomlkit
 
 workspace_file = "workspace.toml"
 root_file = ".git"
@@ -12,6 +15,38 @@ projects_dir = "projects"
 development_dir = "development"
 
 
+def load_content(fullpath: Path) -> tomlkit.TOMLDocument:
+    content = fullpath.read_text()
+
+    return tomlkit.loads(content)
+
+
+@lru_cache
+def load_root_project_config(path: Path) -> tomlkit.TOMLDocument:
+    fullpath = path / default_toml
+
+    return load_content(fullpath)
+
+
+def has_workspace_config(data: tomlkit.TOMLDocument) -> bool:
+    ns = data.get("tool", {}).get("polylith", {}).get("namespace")
+
+    return True if ns else False
+
+
+@lru_cache
+def load_workspace_config(path: Path) -> tomlkit.TOMLDocument:
+    fullpath = path / workspace_file
+
+    if fullpath.exists():
+        content = load_content(fullpath)
+
+        if has_workspace_config(content):
+            return content
+
+    return load_root_project_config(path)
+
+
 def is_drive_root(cwd: Path) -> bool:
     return cwd == Path(cwd.root) or cwd == cwd.parent
 
@@ -22,6 +57,12 @@ def is_repo_root(cwd: Path) -> bool:
     return fullpath.exists()
 
 
+def is_python_workspace_root(path: Path) -> bool:
+    data = load_root_project_config(path)
+
+    return has_workspace_config(data)
+
+
 def find_upwards(cwd: Path, name: str) -> Union[Path, None]:
     if is_drive_root(cwd):
         return None
@@ -29,7 +70,10 @@ def find_upwards(cwd: Path, name: str) -> Union[Path, None]:
     fullpath = cwd / name
 
     if fullpath.exists():
-        return fullpath
+        if name == workspace_file:
+            return fullpath
+
+        return fullpath if is_python_workspace_root(cwd) else None
 
     if is_repo_root(cwd):
         return None
@@ -45,9 +89,13 @@ def find_upwards_dir(cwd: Path, name: str) -> Union[Path, None]:
 
 def find_workspace_root(cwd: Path) -> Union[Path, None]:
     workspace_root = find_upwards_dir(cwd, workspace_file)
+
     if workspace_root:
         return workspace_root
-    return find_upwards_dir(cwd, root_file)
+
+    repo_root = find_upwards_dir(cwd, root_file)
+
+    return repo_root or find_upwards_dir(cwd, default_toml)
 
 
 def get_workspace_root(cwd: Path) -> Path:
@@ -55,7 +103,7 @@ def get_workspace_root(cwd: Path) -> Path:
 
     if not root:
         raise ValueError(
-            "Didn't find the workspace root. Expected to find a workspace.toml or .git file."
+            "Didn't find the workspace root. Expected to find a workspace.toml or pyproject.toml with Workspace config."
         )
 
     return root
