46127e1a852a1c7f13215b5ac02ef663aceffdd0
David Vujic <david@vujic.se>
2024-12-03 20:26:35 +0100

    fix(hatchling): filter project-specific bricks when in editable mode and with dev-mode-dirs enabled

--- components/polylith/hatch/hooks/bricks.py
+++ components/polylith/hatch/hooks/bricks.py
@@ -1,22 +1,49 @@
 import shutil
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from polylith import parsing, repo, toml
 from polylith.hatch import core
 
 
+def get_build_section(data: dict) -> dict:
+    return data.get("tool", {}).get("hatch", {}).get("build", {})
+
+
+def is_in_path(key: str, paths: List[str]) -> bool:
+    return any(key.startswith(p) for p in paths)
+
+
+def filter_dev_mode_bricks(data: dict, bricks: dict) -> dict:
+    build_section = get_build_section(data)
+    dev_mode_dirs = build_section.get("dev-mode-dirs")
+
+    if not dev_mode_dirs:
+        return bricks
+
+    return {k: v for k, v in bricks.items() if not is_in_path(k, dev_mode_dirs)}
+
+
+def filtered_bricks(data: dict, version: str) -> dict:
+    bricks = toml.get_project_packages_from_polylith_section(data)
+
+    if version == "editable":
+        return filter_dev_mode_bricks(data, bricks)
+
+    return bricks
+
+
 class PolylithBricksHook(BuildHookInterface):
     PLUGIN_NAME = "polylith-bricks"
 
     def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
-        include_key = "force_include_editable" if version == "editable" else "force_include"
+        include_key = "force_include"
         root = self.root
         pyproject = Path(f"{root}/{repo.default_toml}")
 
         data = toml.read_toml_document(pyproject)
-        bricks = toml.get_project_packages_from_polylith_section(data)
+        bricks = filtered_bricks(data, version)
         found_bricks = {k: v for k, v in bricks.items() if Path(f"{root}/{k}").exists()}
 
         if not bricks or not found_bricks:
