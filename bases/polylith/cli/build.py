d721e57d92cfcbe5ee50da6b68e49390f80ac058
David Vujic <david@vujic.se>
2024-12-06 22:19:03 +0100

    fix(poly build setup and teardown): proper build paths

--- bases/polylith/cli/build.py
+++ bases/polylith/cli/build.py
@@ -9,14 +9,15 @@ from typing_extensions import Annotated
 app = Typer()
 
 
-def get_work_dir(directory: str) -> Path:
+def get_work_dir(root: Path, directory: str) -> Path:
     work_dir = building.get_work_dir({})
+    work_path = Path(directory) / work_dir if directory else work_dir
 
-    return Path(directory) / work_dir
+    return root / work_path
 
 
-def get_build_dir(directory: str) -> Path:
-    return Path(directory) if directory else Path.cwd()
+def get_build_dir(root: Path, directory: str) -> Path:
+    return root / Path(directory) if directory else root
 
 
 def get_project_data(build_dir: Path) -> tomlkit.TOMLDocument:
@@ -34,8 +35,10 @@ def setup_command(directory: Annotated[str, options.directory] = ""):
     Run it before the build command of your Package & Dependency Management tool.
 
     """
-    work_dir = get_work_dir(directory)
-    build_dir = get_build_dir(directory)
+    root = Path.cwd()
+
+    work_dir = get_work_dir(root, directory)
+    build_dir = get_build_dir(root, directory)
 
     data = get_project_data(build_dir)
     bricks = toml.get_project_packages_from_polylith_section(data)
@@ -43,19 +46,24 @@ def setup_command(directory: Annotated[str, options.directory] = ""):
     if not bricks:
         return
 
+    bricks_with_paths = {build_dir / k: v for k, v in bricks.items()}
     custom_top_ns = toml.get_custom_top_namespace_from_polylith_section(data)
 
     if not custom_top_ns:
-        building.copy_bricks_as_is(bricks, build_dir)
+        building.copy_bricks_as_is(bricks_with_paths, build_dir)
     else:
-        building.copy_and_rewrite_bricks(bricks, custom_top_ns, work_dir, build_dir)
+        building.copy_and_rewrite_bricks(
+            bricks_with_paths, custom_top_ns, work_dir, build_dir
+        )
 
 
 @app.command("teardown")
 def teardown_command(directory: Annotated[str, options.directory] = ""):
     """Clean up temporary directories. Run it after the build command of your Package & Dependency Management tool."""
-    work_dir = get_work_dir(directory)
-    build_dir = get_build_dir(directory)
+    root = Path.cwd()
+
+    work_dir = get_work_dir(root, directory)
+    build_dir = get_build_dir(root, directory)
 
     data = get_project_data(build_dir)
     bricks = toml.get_project_packages_from_polylith_section(data)
@@ -68,4 +76,4 @@ def teardown_command(directory: Annotated[str, options.directory] = ""):
     building.cleanup(work_dir)
 
     if destination_dir:
-        building.cleanup(destination_dir)
+        building.cleanup(build_dir / destination_dir)
