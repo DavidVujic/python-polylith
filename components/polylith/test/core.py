7ebef7dbe9c1a1add493436277a228ce4128b427
David Vujic <david@vujic.se>
2025-05-01 19:58:03 +0200

    wip: collect all test paths

--- components/polylith/test/core.py
+++ components/polylith/test/core.py
@@ -1,26 +1,37 @@
 from pathlib import Path
-from typing import List, Set
+from typing import List, Union
 
 from polylith import check, configuration, diff, info
+from polylith.bricks import get_bases_data, get_components_data
 
 
-def get_projects_affected_by_changes(
-    projects_data: List[dict], brick_imports: dict
-) -> Set[str]:
-    names = set().union(*brick_imports.values())
-    bricks = list(names)
+def is_test(root: Path, ns: str, path: Path, theme: str) -> bool:
+    expected = "test"
+    file_path = path.as_posix()
 
-    return diff.collect.get_projects_affected_by_changes(
-        projects_data, [], bricks, bricks
-    )
-
-
-def is_test(root: Path, ns: str, filepath: Path, theme: str) -> bool:
     if theme == "loose":
-        test_path = root / "test"
-        return str.startswith(filepath.as_posix(), test_path.as_posix())
+        test_path = Path(root / f"{expected}/").as_posix()
 
-    return f"/test/{ns}" in filepath.as_posix()
+        return str.startswith(file_path, test_path)
+
+    return f"/{expected}/{ns}" in file_path
+
+
+def get_changed_files(root: Path, tag_name: Union[str, None]) -> List[Path]:
+    tag = diff.collect.get_latest_tag(root, tag_name) or tag_name
+
+    if not tag:
+        return []
+
+    return [root / f for f in diff.collect.get_files(tag)]
+
+
+def get_brick_imports_in_tests(
+    root: Path, ns: str, theme: str, files: List[Path]
+) -> dict:
+    matched = {f for f in files if is_test(root, ns, f, theme)}
+
+    return check.collect.extract_bricks(matched, ns)
 
 
 def comment():
@@ -30,11 +41,14 @@ def comment():
 
     projects_data = [p for p in info.get_projects_data(root, ns) if info.is_project(p)]
 
-    tag = diff.collect.get_latest_tag(root, None)
-    files = [root / f for f in diff.collect.get_files(tag)]
+    files = get_changed_files(root, None)
+    brick_imports = get_brick_imports_in_tests(root, ns, theme, files)
+    bricks = set().union(*brick_imports.values())
 
-    matched = {f for f in files if is_test(root, ns, f, theme)}
+    all_bases = {v for b in get_bases_data(root, ns) for v in b.values()}
+    all_components = {v for c in get_components_data(root, ns) for v in c.values()}
 
-    brick_imports = check.collect.extract_bricks(matched, ns)
+    bases = [b for b in all_bases if b in bricks]
+    components = [c for c in all_components if c in bricks]
 
-    get_projects_affected_by_changes(projects_data, brick_imports)
+    diff.collect.get_projects_affected_by_changes(projects_data, [], bases, components)
