c4fc5108342583adc0082f6a796883eb815523f8
David Vujic <david@vujic.se>
2025-05-01 17:47:34 +0200

    wip: collect all test paths

--- components/polylith/test/core.py
+++ components/polylith/test/core.py
@@ -1,8 +1,7 @@
-from functools import reduce
 from pathlib import Path
 from typing import List, Set
 
-from polylith import check, diff, info, workspace
+from polylith import check, configuration, diff, info
 
 
 def get_brick_imports(root: Path, ns: str, paths: Set[Path]) -> dict:
@@ -24,36 +23,27 @@ def get_projects_affected_by_changes(
     )
 
 
-def append_bricks(acc: Set[str], brick_type: str, project_data: dict) -> Set[str]:
-    bricks = project_data.get(brick_type, [])
+def is_test(root: Path, ns: str, filepath: Path, theme: str) -> bool:
+    if theme == "loose":
+        test_path = root / "test"
+        return str.startswith(filepath.as_posix(), test_path.as_posix())
 
-    return acc.union(bricks)
-
-
-def collect_bases(acc: Set[str], project_data: dict) -> Set[str]:
-    return append_bricks(acc, "bases", project_data)
-
-
-def collect_components(acc: Set[str], project_data: dict) -> Set[str]:
-    return append_bricks(acc, "components", project_data)
+    return f"/test/{ns}" in filepath.as_posix()
 
 
 if False:
     root = Path.cwd()
     ns = "polylith"
 
+    theme = configuration.get_theme_from_config(root)
+
     projects_data = [p for p in info.get_projects_data(root, ns) if info.is_project(p)]
 
-    bases: Set[str] = reduce(collect_bases, projects_data, set())
-    components: Set[str] = reduce(collect_components, projects_data, set())
+    tag = diff.collect.get_latest_tag(root, None)
+    files = [root / f for f in diff.collect.get_files(tag)]
 
-    # TODO: this is very strict, assuming all tests are structured as the "default"
-    bases_tests = workspace.paths.collect_bases_tests_paths(root, ns, bases)
-    components_tests = workspace.paths.collect_components_tests_paths(root, ns, components)
-    tests = bases_tests.union(components_tests)
+    matched = {f for f in files if is_test(root, ns, f, theme)}
 
-    # TODO: compare tests with changed tests
-
-    brick_imports = get_brick_imports(root, ns, tests)
+    brick_imports = get_brick_imports(root, ns, matched)
 
     get_projects_affected_by_changes(projects_data, brick_imports)
