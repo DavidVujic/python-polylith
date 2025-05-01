699243c5d6c6905599db6392ec16f27a5b6c6eb7
David Vujic <david@vujic.se>
2025-05-01 21:18:27 +0200

    wip: add test command

--- components/polylith/test/__init__.py
+++ components/polylith/test/__init__.py
@@ -1,3 +1,4 @@
+from polylith.test.core import get_brick_imports_in_tests, get_changed_files
 from polylith.test.tests import create_test
 
-__all__ = ["create_test"]
+__all__ = ["create_test", "get_brick_imports_in_tests", "get_changed_files"]
--- components/polylith/test/core.py
+++ components/polylith/test/core.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from typing import List, Union
 
-from polylith import check, configuration, diff, info
-from polylith.bricks import get_bases_data, get_components_data
+from polylith import check, diff
 
 
 def is_test(root: Path, ns: str, path: Path, theme: str) -> bool:
@@ -32,23 +31,3 @@ def get_brick_imports_in_tests(
     matched = {f for f in files if is_test(root, ns, f, theme)}
 
     return check.collect.extract_bricks(matched, ns)
-
-
-def comment():
-    root = Path.cwd()
-    ns = configuration.get_namespace_from_config(root)
-    theme = configuration.get_theme_from_config(root)
-
-    projects_data = [p for p in info.get_projects_data(root, ns) if info.is_project(p)]
-
-    files = get_changed_files(root, None)
-    brick_imports = get_brick_imports_in_tests(root, ns, theme, files)
-    bricks = set().union(*brick_imports.values())
-
-    all_bases = {v for b in get_bases_data(root, ns) for v in b.values()}
-    all_components = {v for c in get_components_data(root, ns) for v in c.values()}
-
-    bases = [b for b in all_bases if b in bricks]
-    components = [c for c in all_components if c in bricks]
-
-    diff.collect.get_projects_affected_by_changes(projects_data, [], bases, components)
