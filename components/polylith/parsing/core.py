1787a353affab39fbda4c244fd7d5c94304fa030
David Vujic <david@vujic.se>
2025-06-04 21:00:58 +0200

    feat(hatch): add support for configured exclude paths pattern (#339)
    * feat(hatch): add support for configured exclude paths pattern when building wheels and sdists
    
    * feat(build): add .ruff_cache to default exclude pattern
    
    * bump hatch hook to 1.4.0
    
    * bump PDM hooks to 1.2.0
    
    * bump CLI to 1.29.0

--- components/polylith/parsing/core.py
+++ components/polylith/parsing/core.py
@@ -1,28 +1,39 @@
 import shutil
 from pathlib import Path
+from typing import Union
+
+default_patterns = {
+    "*.pyc",
+    "__pycache__",
+    ".venv",
+    "__pypackages__",
+    ".mypy_cache",
+    ".ruff_cache",
+    ".pytest_cache",
+    "node_modules",
+    ".git",
+}
 
 
-def copy_tree(source: str, destination: str) -> Path:
-    ignore = shutil.ignore_patterns(
-        "*.pyc",
-        "__pycache__",
-        ".venv",
-        "__pypackages__",
-        ".mypy_cache",
-        ".pytest_cache",
-        "node_modules",
-        ".git",
-    )
+def copy_tree(source: str, destination: str, patterns: set) -> Path:
+    ignore = shutil.ignore_patterns(*patterns)
 
     res = shutil.copytree(source, destination, ignore=ignore, dirs_exist_ok=True)
 
     return Path(res)
 
 
-def copy_brick(source: str, brick: str, destination_dir: Path) -> Path:
+def copy_brick(
+    source: str,
+    brick: str,
+    destination_dir: Path,
+    exclude_patterns: Union[set, None] = None,
+) -> Path:
     destination = Path(destination_dir / brick).as_posix()
 
-    return copy_tree(source, destination)
+    patterns = set().union(default_patterns, exclude_patterns or set())
+
+    return copy_tree(source, destination, patterns)
 
 
 def parse_brick_namespace_from_path(bricks: dict) -> str:
