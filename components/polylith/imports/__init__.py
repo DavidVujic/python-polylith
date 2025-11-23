8dbdc97bc4acef27bf163245d2e88c38fd92bf42
David Vujic <david@vujic.se>
2025-11-23 11:32:22 +0100

    wip

--- components/polylith/imports/__init__.py
+++ components/polylith/imports/__init__.py
@@ -1,6 +1,7 @@
 from polylith.imports.parser import (
     extract_top_ns,
     fetch_all_imports,
+    fetch_api,
     fetch_excluded_imports,
     list_imports,
 )
@@ -8,6 +9,7 @@ from polylith.imports.parser import (
 __all__ = [
     "extract_top_ns",
     "fetch_all_imports",
+    "fetch_api",
     "fetch_excluded_imports",
     "list_imports",
 ]
