e79108f988ef0f94f6e8327d154bd8c61317d79e
David Vujic <david@vujic.se>
2026-04-06 09:47:42 +0200

    feat: add support for project name alias, and project grouping

--- components/polylith/configuration/core.py
+++ components/polylith/configuration/core.py
@@ -1,35 +1,49 @@
 from pathlib import Path
-from typing import List, Union
+from typing import List, Set, Union
 
 from polylith import repo
 
 
-def get_namespace_from_config(path: Path) -> str:
+def get_polylith_config(path: Path) -> dict:
     toml: dict = repo.load_workspace_config(path)
 
-    return toml["tool"]["polylith"]["namespace"]
+    conf = toml.get("tool", {}).get("polylith")
+
+    if not conf:
+        message = "Cannot find a [tool.polylith] configuration."
+        raise ValueError(message)
+
+    return conf
 
 
-def get_git_tag_pattern(toml: dict) -> str:
-    """Fallback git tag pattern configuration"""
-    return toml["tool"]["polylith"]["git_tag_pattern"]
+def get_namespace_from_config(path: Path) -> str:
+    conf = get_polylith_config(path)
+    ns = conf.get("namespace")
+
+    if not ns:
+        message = "Cannot find a [tool.polylith.namespace] section."
+        raise ValueError(message)
+
+    return ns
 
 
 def get_tag_pattern_from_config(path: Path, key: Union[str, None]) -> Union[str, None]:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
 
-    patterns = toml["tool"]["polylith"].get("tag", {}).get("patterns")
+    patterns = conf.get("tag", {}).get("patterns")
+    fallback = conf.get("git_tag_pattern")
 
     if not key:
-        return patterns["stable"] if patterns else get_git_tag_pattern(toml)
+        stable = patterns.get("stable") if patterns else None
+        return stable or fallback
 
-    return patterns.get(key)
+    return patterns.get(key) if patterns else None
 
 
 def get_tag_sort_options_from_config(path: Path) -> List[str]:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
 
-    options = toml["tool"]["polylith"].get("tag", {}).get("sorting")
+    options = conf.get("tag", {}).get("sorting")
     # Default sorting option
     if options is None:
         return ["-committerdate"]
@@ -37,23 +51,23 @@ def get_tag_sort_options_from_config(path: Path) -> List[str]:
 
 
 def is_test_generation_enabled(path: Path) -> bool:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
+    enabled = conf.get("test", {}).get("enabled")
 
-    enabled = toml["tool"]["polylith"]["test"]["enabled"]
     return bool(enabled)
 
 
 def is_readme_generation_enabled(path: Path) -> bool:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
+    enabled = conf.get("resources", {}).get("brick_docs_enabled")
 
-    enabled = toml["tool"]["polylith"].get("resources", {}).get("brick_docs_enabled")
     return bool(enabled)
 
 
 def get_theme_from_config(path: Path) -> str:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
 
-    return toml["tool"]["polylith"]["structure"].get("theme") or "tdd"
+    return conf.get("structure", {}).get("theme") or "tdd"
 
 
 def get_brick_structure_from_config(path: Path) -> str:
@@ -84,15 +98,38 @@ def get_resources_structure_from_config(path: Path) -> str:
 
 
 def get_output_dir(path: Path, command_name: str) -> str:
-    toml: dict = repo.load_workspace_config(path)
+    conf = get_polylith_config(path)
 
     key = "output"
 
-    tool = toml["tool"]["polylith"]
-    commands = tool.get("commands", {})
-    command = commands.get(command_name, {})
+    commands = conf.get("commands") or {}
+    command = commands.get(command_name) or {}
 
     output = command.get(key) or commands.get(key)
     fallback = f"{repo.development_dir}/poly"
 
     return output or fallback
+
+
+def get_projects_config(path: Path) -> dict:
+    conf = get_polylith_config(path)
+
+    return conf.get("projects", {})
+
+
+def get_project_alias_from_config(path: Path, project_name: str) -> Union[str, None]:
+    projects_conf = get_projects_config(path)
+    alias = projects_conf.get("alias", {})
+
+    return alias.get(project_name)
+
+
+def get_project_groups_from_config(path: Path, project_name: str) -> Set[str]:
+    projects_conf = get_projects_config(path)
+
+    groups = projects_conf.get("groups") or {}
+
+    if not groups:
+        return set()
+
+    return {k for k, v in groups.items() if project_name in v}
