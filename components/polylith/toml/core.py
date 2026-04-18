f7d2353cfff84f4dab114955f81359e968ad4107
David Vujic <david@vujic.se>
2026-04-18 16:34:37 +0200

    fix(poly info): for Poetry with PEP-621 support (#440)
    * fix(poly info): for Poetry with PEP-621 support
    
    * bump Hatch hook to 1.5.6
    
    * bump PDM bricks hook to 1.3.9
    
    * bump PDM workspace hook to 1.3.9
    
    * bump Poetry plugin to 1.51.3
    
    * bump CLI to 1.45.3

--- components/polylith/toml/core.py
+++ components/polylith/toml/core.py
@@ -77,7 +77,7 @@ def collect_configured_pdm_exclude_patterns(data: dict) -> set:
 
 
 def collect_configured_poetry_exclude_patterns(data: dict) -> set:
-    exclude = data["tool"]["poetry"].get("exclude", [])
+    exclude = data.get("tool", {}).get("poetry", {}).get("exclude", [])
 
     return set(exclude)
 
@@ -111,8 +111,12 @@ def collect_configured_exclude_patterns(
     return set()
 
 
+def is_poetry_without_pep_621_support(data) -> bool:
+    return repo.is_poetry(data) and not repo.is_pep_621_ready(data)
+
+
 def get_project_package_includes(namespace: str, data) -> List[dict]:
-    if repo.is_poetry(data):
+    if is_poetry_without_pep_621_support(data):
         return data["tool"]["poetry"].get("packages", [])
 
     includes = (
@@ -170,10 +174,6 @@ def get_pep_621_optional_dependencies(data) -> List[str]:
     matrix = list(groups.values()) if isinstance(groups, dict) else []
 
     return sum(matrix, [])
-
-
-def is_poetry_without_pep_621_support(data) -> bool:
-    return repo.is_poetry(data) and not repo.is_pep_621_ready(data)
 
 
 def parse_project_dependencies(data) -> dict:
