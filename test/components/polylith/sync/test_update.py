be4bfe56e09382ac80b36e5aa661c34696002561
David Vujic <david@vujic.se>
2026-07-27 12:27:11 +0200

    fix(poly sync): sorting the bricks to update in the pyproject.toml

--- test/components/polylith/sync/test_update.py
+++ test/components/polylith/sync/test_update.py
@@ -1,7 +1,14 @@
 from pathlib import Path
+from typing import Union
 
 import tomlkit
 from polylith.sync import update
+
+
+def parse(data: Union[str, None]) -> dict:
+    parsed = tomlkit.parse(data) if data else {}
+
+    return parsed or {}
 
 
 def test_brick_to_pyproject_package():
@@ -70,10 +77,20 @@ expected_hatch_packages = {
     "components/hello/third": "hello/third",
 }
 
+unsorted_packages = [
+    {"include": "hello/c", "from": "components"},
+    {"include": "hello/a", "from": "bases"},
+    {"include": "hello/b", "from": "components"},
+]
 
-def test_generate_updated_poetry_project():
-    data = tomlkit.parse(
-        """\
+expected_sorted_pep621_bricks = {
+    "bases/hello/first": "hello/first",
+    "bases/hello/a": "hello/a",
+    "components/hello/b": "hello/b",
+    "components/hello/c": "hello/c",
+}
+
+poetry_project_data = """\
 [tool.poetry]
 packages = [{include = "hello/first", from = "bases"}]
 
@@ -81,64 +98,105 @@ packages = [{include = "hello/first", from = "bases"}]
 requires = ["poetry-core>=1.0.0"]
 build-backend = "poetry.core.masonry.api"
 """
-    )
+
+
+hatchling_build_system = """\
+[build-system]
+requires = ["hatchling"]
+build-backend = "hatchling.build"
+"""
+
+hatchling_project_data = """\
+{build_system}
+
+[tool.polylith.bricks]
+"bases/hello/first" = "hello/first"
+""".format(
+    build_system=hatchling_build_system
+)
+
+hatch_specific_project_data = """\
+{build_system}
+
+[tool.hatch.build.force-include]
+"bases/hello/first" = "hello/first"
+""".format(
+    build_system=hatchling_build_system
+)
+
+
+def test_generate_updated_poetry_project():
+    data = tomlkit.parse(poetry_project_data)
 
     updated = update.generate_updated_project(data, packages[1:])
 
-    res = tomlkit.parse(updated)["tool"]["poetry"]["packages"]
+    res = parse(updated)["tool"]["poetry"]["packages"]
 
     assert res == packages
 
 
-def test_generate_updated_hatch_project_with_existing_polylith_sections():
-    data = tomlkit.parse(
-        """\
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
+def test_generate_updated_poetry_project_with_the_bricks_to_update_sorted():
+    data = tomlkit.parse(poetry_project_data)
 
-[tool.polylith.bricks]
-"bases/hello/first" = "hello/first"
-"""
-    )
+    expected = [
+        {"include": "hello/first", "from": "bases"},
+        {"include": "hello/a", "from": "bases"},
+        {"include": "hello/b", "from": "components"},
+        {"include": "hello/c", "from": "components"},
+    ]
+
+    updated = update.generate_updated_project(data, unsorted_packages)
+
+    res = parse(updated)["tool"]["poetry"]["packages"]
+
+    assert res == expected
+
+
+def test_generate_updated_hatch_project_with_existing_polylith_sections():
+    data = tomlkit.parse(hatchling_project_data)
 
     updated = update.generate_updated_project(data, packages[1:])
 
-    res = tomlkit.parse(updated)["tool"]["polylith"]["bricks"]
+    res = parse(updated)["tool"]["polylith"]["bricks"]
 
     assert res == expected_hatch_packages
 
 
+def test_generate_updated_pep621_project_with_the_bricks_to_update_sorted():
+    data = tomlkit.parse(hatchling_project_data)
+
+    updated = update.generate_updated_project(data, unsorted_packages)
+
+    res = parse(updated)["tool"]["polylith"]["bricks"]
+
+    assert list(res.keys()) == list(expected_sorted_pep621_bricks.keys())
+
+
 def test_generate_updated_hatch_project_with_missing_brick_config():
-    data = tomlkit.parse(
-        """\
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
-"""
-    )
+    data = tomlkit.parse(hatchling_build_system)
 
     updated = update.generate_updated_project(data, packages)
 
-    res = tomlkit.parse(updated)["tool"]["polylith"]["bricks"]
+    res = parse(updated)["tool"]["polylith"]["bricks"]
 
     assert res == expected_hatch_packages
 
 
 def test_generate_updated_hatch_project_with_existing_force_include():
-    data = tomlkit.parse(
-        """\
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
-
-[tool.hatch.build.force-include]
-"bases/hello/first" = "hello/first"
-"""
-    )
+    data = tomlkit.parse(hatch_specific_project_data)
 
     updated = update.generate_updated_project(data, packages[1:])
 
-    res = tomlkit.parse(updated)["tool"]["hatch"]["build"]["force-include"]
+    res = parse(updated)["tool"]["hatch"]["build"]["force-include"]
 
     assert res == expected_hatch_packages
+
+
+def test_generate_updated_hatch_project_force_include_with_sorted_bricks():
+    data = tomlkit.parse(hatch_specific_project_data)
+
+    updated = update.generate_updated_project(data, unsorted_packages)
+
+    res = parse(updated)["tool"]["hatch"]["build"]["force-include"]
+
+    assert list(res.keys()) == list(expected_sorted_pep621_bricks.keys())
