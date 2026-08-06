5b2e4715bda30669dc26b46988ee6c60782ad6a1
David Vujic <david@vujic.se>
2026-08-06 08:20:57 +0200

    test(poly test diff): generic variable and path name

--- test/components/polylith/test/test_core.py
+++ test/components/polylith/test/test_core.py
@@ -4,10 +4,10 @@ from polylith.test import core
 
 
 def test_extract_brick_name_from_test() -> None:
-    expected = "check"
+    expected = "hello_world"
     root = Path.cwd()
 
-    changed_test = root / f"test/components/polylith/{expected}/the_test.py"
+    changed_test = root / f"test/components/my_namespace/{expected}/the_test.py"
 
     res = core.extract_brick_name_from_test(root, changed_test, theme="loose")
 
